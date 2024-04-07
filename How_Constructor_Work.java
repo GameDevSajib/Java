e5b8b04e38c441f847e4686eac71a12b3eb07992
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2024-04-07 02:10:19 -0700

    Add files via upload

---
+++ How_Constructor_Work.java
@@ -0,0 +1,16 @@
+
+public  class How_Constructor_Work {
+	
+	
+	int a=10;
+	int b=50;
+	///This is the custom contractor of our own in java it works basically by default  
+	 public How_Constructor_Work() {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
