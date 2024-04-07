42c016ff0e10064eb45e7edd88dce4318f5793fb
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2024-04-07 03:15:08 -0700

    Add files via upload

---
+++ Constructor_Overloading.java
@@ -0,0 +1,29 @@
+
+public class Constructor_Overloading {
+	
+	private int id;
+	private String name;
+	private int age;
+	
+	public Constructor_Overloading()
+	{
+		
+	}
+	
+	 public Constructor_Overloading(int id)
+	{
+		this.id=id;
+	}
+	 
+	 public Constructor_Overloading(int id,String name,int age)
+		{
+			this.id=id;
+			this.name=name;
+			this.age=age;
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
